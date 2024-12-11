16dc4f1466998c1f0d724053626efa22076997b8
Guillaume Massol <guillaume.massol@gmail.com>
2024-12-11 22:31:39 +0100

    Add command line arg to show hidden files

--- jupyter/jupyter_start.sh
+++ jupyter/jupyter_start.sh
@@ -1,4 +1,5 @@
 start-notebook.py --ip=0.0.0.0 \
   --PasswordIdentityProvider.hashed_password='' \
   --IdentityProvider.token='' \
-  --ServerApp.root_dir='/home/jovyan/work'
+  --ServerApp.root_dir='/home/jovyan/work' \
+  --ContentsManager.allow_hidden = True
